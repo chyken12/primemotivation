95efe5278f1b232d3c3732d3d36588ac0bbe72cf
sammy <chyken07@gmail.com>
2022-11-30 23:34:22 +0000

    db changed to postgresql

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -136,10 +136,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = "/media/"
 # Default primary key field type
