992097ea395e6d64d2d4b7640b3dc27da1f2287e
sammy <chyken07@gmail.com>
2022-12-01 21:24:52 +0000

    staticstorage commented

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -142,7 +142,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = "/media/"
